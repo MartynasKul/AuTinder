c5a39217086da074012750d70c1b3396de9c77db
Edvinas <126004821+edvral@users.noreply.github.com>
2024-04-24 19:49:57 +0300

    Fixed

--- AuTinder/Program.cs
+++ AuTinder/Program.cs
@@ -4,33 +4,51 @@ namespace AuTinder
     {
         public static void Main(string[] args)
         {
+
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddHttpContextAccessor();
+
+
+            builder.Services.AddDistributedMemoryCache();
+            // Add session middleware
+            builder.Services.AddSession(options =>
+            {
+                // Set a timeout for the session
+                options.IdleTimeout = TimeSpan.FromSeconds(3600);
+            });
+
+
+            // Add HttpContextAccessor
+            builder.Services.AddHttpContextAccessor();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            // Use session middleware
+
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Route/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
-            app.UseRouting();
-
             app.UseAuthorization();
+            app.UseRouting();
+            app.UseSession();
+
 
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Route}/{action=Index}/{id?}");
 
+            Config.CreateSingletonInstance(app.Configuration);
+
+
             app.Run();
+
         }
     }
 }
