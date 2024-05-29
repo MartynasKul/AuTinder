8732306073c42a06633c4a0ec55b8eb8c17b8b3e
Martynas Kuliešius <martiss200@gmail.com>
2024-05-29 23:18:40 +0300

    Perziureti pristatymu pasiulymus lmao
    i have c

---
+++ AuTinder/Models/SeenDelivery.cs
@@ -0,0 +1,21 @@
+﻿namespace AuTinder.Models
+{
+    public class SeenDelivery
+    {
+        public int DeliveryID { get; set; }
+        public int UserID { get; set; }
+        public bool Liked {  get; set; }
+        public Delivery delivery { get; set; }
+
+        public SeenDelivery() { }
+
+        public SeenDelivery(int deliveryID, int userID, bool liked, Delivery delivery)
+        {
+            DeliveryID = deliveryID;
+            UserID = userID;
+            Liked = liked;
+            this.delivery = delivery;
+        }
+
+    }
+}
